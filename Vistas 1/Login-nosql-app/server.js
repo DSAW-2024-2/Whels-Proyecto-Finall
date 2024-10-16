795a9a397ccfe41d00dc732fdd740cc6653029e1
santibari <santiagobari@unisabana.edu.co>
2024-10-15 19:57:53 -0500

    otro arreglo mas

--- Vistas 1/Login-nosql-app/server.js
+++ Vistas 1/Login-nosql-app/server.js
@@ -10,7 +10,7 @@ const port = 3000;
 app.use(bodyParser.json());
 
 // Conectar a MongoDB (local o MongoDB Atlas)
-mongoose.connect('mongodb://localhost:27017/local', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/local/usuarios', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Conectado a MongoDB'))
     .catch(err => console.error('No se pudo conectar a MongoDB', err));
 
