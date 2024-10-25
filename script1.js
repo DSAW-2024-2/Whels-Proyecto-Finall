f5eab824b15c208a4b7af8a24d60b2b279d33d6d
santibari <santiagobari@unisabana.edu.co>
2024-10-24 23:55:26 -0500

    2

--- script1.js
+++ script1.js
@@ -1,4 +1,5 @@
-// Redirigir a index2.html (login) después de 3 segundos
-setTimeout(() => {
-    window.location.href = "Login-nosql-app/frontend/html/index3.html";
-}, 3000);
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        window.location.href = "/Login-nosql-app/frontend/html/index3.html";  // O la ruta que necesites
+    }, 3000);
+});
